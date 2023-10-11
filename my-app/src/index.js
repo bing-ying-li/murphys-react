5f8218c3315196dcbbb379de2321c4a78029dd97
Derealization-kk <bingying.li02@gmail.com>
2023-10-10 23:07:11 -1000

    Not done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import { Container } from 'react-bootstrap';
+import { Container, Nav, Image } from 'react-bootstrap';
+
+const TopMenu = () => (
+  <Container id = "topMenu">
+    <Nav className="me-auto justify-content-start">
+      <Image src = "http://courses.ics.hawaii.edu/ics314f22/morea/ui-frameworks/murphyslogowhite.png" width="200px"></Image>
+    </Nav>
+    <Nav className="justify-content-end">
+
+    </Nav>
+  </Container>
+);
+
+const IslandSnowLogo = () => (
+  <h1>IslandSnowLogo</h1>
+);
+
+const MiddleMenu = () => (
+  <h1>MiddleMenu</h1>
+);
+
+const FullWidthImage = () => (
+  <h1>FullWidthImage</h1>
+);
+
+const FooterMenu = () => (
+  <h1>FooterMenu</h1>
+);
 
 const Murphys = () => (
   <Container>
-    <h1 className="text-center">Murphy's</h1>
+    <TopMenu/>
+    <IslandSnowLogo/>
+    <MiddleMenu/>
+    <FullWidthImage/>
+    <FooterMenu/>
   </Container>
 );
 
